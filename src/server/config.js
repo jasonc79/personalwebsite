b3d905d268cdb52eae730a6f4fae19fe76dcc4b2
Jason Cheung <z5360652@ad.unsw.edu.au>
2024-08-03 01:27:58 +1000

    Created a backend server to handle email message request to send to my home email address

---
+++ src/server/config.js
@@ -0,0 +1,8 @@
+// change the following details with your information
+export const USER = 'jcheung379@gmail.com';
+export const PASS = 'wmeeaahsfkzcxosu';
+export const EMAIL = 'jcheung379@gmail.com';
+export const HOST = 'smtp.gmail.com';
+export const MAILPORT = 465;
+export const YOURNAME = 'Jason Cheung';
+export const YOURSITE = 'www.google.com';
